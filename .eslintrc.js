f672ddd04530bd0d251cd25823887e6d39bd3036
topu <11410347+topuhome_0@user.noreply.gitee.com>
2023-07-02 11:34:15 +0800

    配置组件库的基本样式

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true
+  },
+  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2021,
+    "sourceType": "module"
+  },
+  "plugins": ["@typescript-eslint"],
+  "rules": {}
+  
+  
+}
